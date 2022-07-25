b7a2242e2653c15745f191e2b2419726a0e2bff2
CodeShala <109964477+CodeShala-Tutorials@users.noreply.github.com>
2022-07-25 14:06:25 +0100

    Add files via upload
    As the venv file has a massive amount of files, I'm unable to upload it inside this GitHub repository, please download this repository and copy these files inside your project file and delete your default main.py and work inside the main.py provided.

---
+++ main.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+bkcolor = "#0a0426"
+detail_bg_color = "#252625"
+
+window_size = {
+    "x": 500,
+    "y": 750
+}
+
+photo_canvas_pos = {
+    "x": 110,
+    "y": 10
+}
+
+detail_bg_pos = {
+    "x": 30,
+    "y": 330
+}
+
+details_bg_size = {
+    "width": 440,
+    "height": 350
+}
+
+
+email_label_pos = {
+    "x": 70,
+    "y": 75
+}
+
+email_entry_pos = {
+    "x": 160,
+    "y": 400
+}
+
+password_label_pos = {
+    "x": 80,
+    "y": 210
+}
+
+password_entry_pos = {
+    "x": 160,
+    "y": 530
+}
+
+enter_button_pos = {
+    "x": 160,
+    "y": 640
+}
